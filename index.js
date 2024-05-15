56fce4b2fa1f018ff4bbc35eccfb7cc242a13f93
juliArodrigues4 <juliarodriguescosta07@gmail.com>
2024-05-14 22:40:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//  criar app
+
+const app = express();
+
+//  configuração da API para ler em json
+
+app.use(express.urlencoded({
+    extended: true,
+}))
+app.use(express.json());
+
+// conexao com o banco de dados
+
+let bd = 'mongodb://localhost:27017';
+
+mongoose.connect(bd)
+.then(()=>{
+    console.log('o banco tá conectado pai');
+    app.get('/', (rep, res)=>{
+        res.json({message: 'oi mundo'});
+    })
+}).catch((error)=>{
+    console.log('não conectou ;(');
+})
+
+//  declarar a API como pública
+
+app.listen(3000)
